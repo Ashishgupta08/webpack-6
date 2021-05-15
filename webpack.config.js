f256a4190546212fe93b8bdbed4349e5704e9fc7
Ashish Gupta <59931129+Ashishgupta08@users.noreply.github.com>
2021-05-15 19:44:40 +0530

    init

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,30 @@ const webpackConfig = {
 		path: path.resolve(__dirname, "dist")
 	},
 
-	mode: "production"
+	mode: "production",
+
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules)/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-env"]
+					}
+				}
+			},
+			{
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader"]
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: "asset"
+			}
+		]
+	},
 };
 
 module.exports = webpackConfig;
